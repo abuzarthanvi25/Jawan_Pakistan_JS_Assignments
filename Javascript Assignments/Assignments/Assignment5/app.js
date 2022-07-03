61b7e5bc3c4dfd1d2abb11bebe8863e69056f0c8
abuzarthanvi25 <abuzarthanvi25@gmail.com>
2022-07-03 13:58:14 +0500

    Q4 to Q8 added

--- Javascript Assignments/Assignments/Assignment5/app.js
+++ Javascript Assignments/Assignments/Assignment5/app.js
@@ -61,4 +61,78 @@ document.write("Value after addition is: " + value + "<br>");
 --value;
 document.write("Value after decrement is: " + value + "<br>");
 value = value%3;
-document.write("The remainder is: " + value + "<br>");
+document.write("The remainder is: " + value + "<br> <br>");
+
+// 4. Cost of one movie ticket is 600 PKR. Write a script to
+// store
+// ticket price in a variable & calculate the cost of buying 5
+// tickets
+// to a movie. Example output:
+var costOfTicket = 600;
+var costOf5Tickets = 600*5;
+document.write("The cost to buy 5 tickets to a movie is " + costOf5Tickets + " PKR" + "<br><br>");
+
+// 5. Write a script to display multiplication table of any
+// number in your browser. E.g
+var num = 4;
+document.write("Table of " + num + "<br>");
+for(i=1; i<11; i++){
+    document.write(num + " x " + i + " = " + num*i + "<br>");
+}
+
+// 6. The Temperature Converter: It’s hot out! Let’s make a
+// converter based on the steps here.
+// a. Store a Celsius temperature into a variable.
+// b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+// c. Now store a Fahrenheit temperature into a variable.
+// d. Convert it to Celsius & output “NNoF is NNoC”.
+var celsiusTemp = 25;
+var farenheitTemp = 70;
+var convertedToF = ((celsiusTemp*9)/5) + 32;
+var convertedToC = ((farenheitTemp - 30))* 5/9;
+document.write("<br>" + celsiusTemp + "\u00B0" + "C" +  " is " + convertedToF + "\u00B0" + "F" + "<br>" );
+document.write(farenheitTemp + "\u00B0" + "F" +  " is " + convertedToC + "\u00B0" + "C" + "<br><br>" )
+
+// 7. Write a program to implement checkout process of a
+// shopping cart system for an e-commerce website. Store
+// the following in variables
+// MATH EXPRESSIONS | JAVASCRIPT
+// Page 5 of 9
+// a. Price of item 1
+// b. Price of item 2
+// c. Ordered quantity of item 1
+// d. Ordered Quantity of item 2
+// e. Shipping charges
+// Compute the total cost & show the receipt in your browser.
+var priceOfItem1 = 650;
+var quantityOfItem1 = 3;
+var priceOfItem2 = 100;
+var quantityOfItem2 = 7;
+var shippingCharges = 100;
+var totalCost = (quantityOfItem1*priceOfItem1) + (quantityOfItem2*priceOfItem2) + shippingCharges
+
+const h1ShoppingCart = document.createElement("h1");
+h1ShoppingCart.textContent = "Shopping Cart \n";
+document.body.append(h1ShoppingCart);
+
+document.write("Price of item 1 is " + priceOfItem1 + "<br>");
+document.write("Quantity of item 1 is " + quantityOfItem1 + "<br>");
+document.write("Price of item 2 is " + priceOfItem2 + "<br>");
+document.write("Quantity of item 2 is " + quantityOfItem2 + "<br>");
+document.write("Shipping charges " + shippingCharges + "<br><br>");
+document.write("Total cost of your order is " + totalCost + "<br><br>");
+
+// 8. Store total marks & marks obtained by a student in 2
+// variables. Compute the percentage & show the result in
+// your browser
+const h1MarksSheet = document.createElement("h1");
+h1MarksSheet.textContent = "Marks Sheet \n";
+document.body.append(h1MarksSheet);
+
+var totalMarks = 980;
+var marksObtained = 804;
+var percentage = (marksObtained/totalMarks) * 100;
+
+document.write("Total Marks: " + totalMarks);
+document.write("Marks Obtained: " + marksObtained);
+document.write("Percentage: " + percentage + "%");
